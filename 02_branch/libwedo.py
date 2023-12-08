49a0ab450fe53054d23ac07be71bebaef2e55010
Pannawit Srisuk <84775119+pannawis@users.noreply.github.com>
2023-12-08 11:01:19 +0700

    Initial commit

---
+++ 02_branch/libwedo.py
@@ -0,0 +1,46 @@
+
+class MathOperate:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def sum(self):
+        result = None
+        return result
+    
+    def subtract(self):
+        return None
+    
+    def multipy(self):
+        return None
+    
+    def divde(self):
+        return None
+    
+    def rms(self):
+        return None
+    
+    def mean(self):
+        return None
+    
+    def std(self):
+        return None
+    
+    def max(self):
+        return None
+    
+    def min(self):
+        return None
+    
+    def show_result(self):
+        print(f"a: {self.a}, b: {self.b}")
+        print(f"sum: {self.sum()}")
+        print(f"subtract: {self.subtract()}")
+        print(f"multiply: {self.multipy()}")
+        print(f"divide: {self.divde()}")
+        print(f"rms: {self.rms()}")
+        print(f"mean: {self.mean()}")
+        print(f"standard derivative: {self.std()}")
+        print(f"max: {self.max()}")
+        print(f"min: {self.min()}")
---
+++ 02_branch/main.py
@@ -0,0 +1,8 @@
+import libwedo
+
+a = 5
+b = 3
+
+MathObj = libwedo.MathOperate(a, b)
+
+MathObj.show_result()
